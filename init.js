4f92f532967d03b5dbb874527294fa0074941a62
shan15dev <shansai@bluewin.ch>
2023-01-16 19:32:38 +0100

    statistics component has been created

--- init.js
+++ init.js
@@ -3,11 +3,20 @@ db = db.getSiblingDB('animalQuiz');
 db.createCollection('animal');
 db.animal.insertMany([
     {
-        "animal" : "Elephant",
-        "maxAge" : 55
+        "animal": "Elephant",
+        "maxAge": 55
     },
     {
-        "animal" : "Monkey",
-        "maxAge" : 10
+        "animal": "Monkey",
+        "maxAge": 10
     }
 ]);
+
+db.createCollection("statistics");
+db.statistics.insertMany([
+    {
+        "name": "Aha",
+        "points": 1,
+        "time": 1237
+    }
+])
