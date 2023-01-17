f0b042c5695cfa36dd3fa8db33cd9d3b1b9d5133
FabioKaelin <supermae206@gmail.com>
2023-01-17 23:01:57 +0000

    i dont know if this works but i hope, and if not its a concept how to ask the questions. if something is wrong/could be better feel free to change it

--- src/main/java/ch/bbw/Main.java
+++ src/main/java/ch/bbw/Main.java
@@ -2,15 +2,6 @@ package ch.bbw;
 
 import ch.bbw.DbServices.UserDBService;
 import ch.bbw.models.Statistics;
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.ServerApi;
-import com.mongodb.ServerApiVersion;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoIterable;
-
 import java.util.Scanner;
 
 public class Main {
@@ -23,5 +14,22 @@ public class Main {
         String name = in.nextLine();
         currentStats.setName(name);
         userDBService.createUser(currentStats);
+        in.close();
+        String[] answers = { "2", "3", "4", "5" };
+        String answer = askQuestion("Wie viele Beine hat eine Katze mit 3 Beinen?", answers);
+        System.out.println(answer);
+    }
+
+    public static String askQuestion(String question, String[] answers) {
+        System.out.println(question);
+        // Arrays.stream(answers).forEach(System.out.println());
+        System.out.println("a: " + answers[0]);
+        System.out.println("b: " + answers[1]);
+        System.out.println("c: " + answers[2]);
+        System.out.println("d: " + answers[3]);
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.nextLine();
+        scanner.close();
+        return answer;
     }
 }
