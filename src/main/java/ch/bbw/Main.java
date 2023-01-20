fbf403b6a44333caf3160144d0f41099f4666ee5
shan15dev <shansai@bluewin.ch>
2023-01-20 19:29:09 +0100

    User is being updated after finishing the Quiz

--- src/main/java/ch/bbw/Main.java
+++ src/main/java/ch/bbw/Main.java
@@ -6,12 +6,9 @@ import ch.bbw.models.Question;
 import ch.bbw.models.Statistics;
 import ch.bbw.models.Topic;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+import java.sql.Timestamp;
+import java.util.*;
 
-import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
 import static ch.bbw.DbServices.UserDBService.getQuestion;
@@ -19,25 +16,34 @@ import static java.lang.Integer.parseInt;
 
 public class Main {
     private static Statistics currentStats = new Statistics();
-    private static UserDBService userDBService = new UserDBService();
-    public static Scanner input = new Scanner(System.in);
+    private static final UserDBService userDBService = new UserDBService();
+    private static Scanner input = new Scanner(System.in);
+    private static int points = 0;
 
     public static void main(String[] args) {
         List<Animal> animals = userDBService.getAnimalsFromDB();
         System.out.println("What's your name?");
-        String name = generateUser(input.nextLine());
-
+        currentStats = generateUser(input.nextLine());
 
         List<Question> questions = getQuestion(askTopic());
         Collections.shuffle(questions);
-        System.out.println();
-        System.out.println("ksldjf");
-        boolean correct = askQuestion(questions.get(0), animals);
-        System.out.println(correct);
+        for (Question question : questions) {
+            askQuestion(question, animals);
+        }
+
+        finishGame();
 
     }
 
-    public static boolean askQuestion(Question question, List<Animal> animals) {
+    private static void finishGame() {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        currentStats.setPoints(points);
+        currentStats.setTime(timestamp.getTime() - currentStats.getTime());
+        currentStats = userDBService.updateUser(currentStats);
+        System.out.println("Congrats! You got " + currentStats.getPoints() + " points in " + currentStats.getTime() + " ms");
+    }
+
+    public static void askQuestion(Question question, List<Animal> animals) {
         System.out.println(question.getQuestion());
         Collections.shuffle(animals);
         System.out.println("0: " + animals.get(0).getAnimal());
@@ -45,17 +51,15 @@ public class Main {
         System.out.println("2: " + animals.get(2).getAnimal());
         System.out.println("Welches Tier trifft zu? ");
         int answer = input.nextInt();
-        List<Integer> propertyList = Arrays.asList(animals.get(0).getProperty(question.getProperty()),
-                animals.get(1).getProperty(question.getProperty()), animals.get(2).getProperty(question.getProperty()));
+        List<Integer> propertyList = Arrays.asList(animals.get(0).getProperty(question.getProperty()), animals.get(1).getProperty(question.getProperty()), animals.get(2).getProperty(question.getProperty()));
         if (question.getCriteria().equals("min")) {
             propertyList = propertyList.stream().sorted().collect(Collectors.toList());
         } else {
             propertyList = propertyList.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         }
         if (animals.get(answer).getProperty(question.getProperty()) == propertyList.get(0)) {
-            return true;
+            points++;
         }
-        return false;
     }
 
 
@@ -66,14 +70,15 @@ public class Main {
             System.out.println("1" + topic.getTopic());
         }
 
-        String topicIndex = input.nextLine();
+        int topicIndex = input.nextInt();
 
-        return topics.get(parseInt(topicIndex) - 1).getTopic();
+        return topics.get(topicIndex - 1).getTopic();
     }
 
-    public static String generateUser(String name) {
+    public static Statistics generateUser(String name) {
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         currentStats.setName(name);
-        userDBService.createUser(currentStats);
-        return name;
+        currentStats.setTime(timestamp.getTime());
+        return userDBService.createUser(currentStats);
     }
 }
